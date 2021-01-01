796cf6d5aa704bca550b06012e6c5b6c72c86d6c
inhdyaii <69579471+inhdyaii@users.noreply.github.com>
2021-01-01 14:07:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ pkg install cowsay -y
 
 apt install figlet -y
 
-gem install lolcat -y
+pkg install lolcat -y
 
 figlet -f big Done !!! | lolcat -a -d 3
 mv MR-HK.cow /data/data/com.termux/files/usr/share/cows
