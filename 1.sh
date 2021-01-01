ab58b805ccbd4263ddbdfec42e36e12009ca51bd
inhdyaii <69579471+inhdyaii@users.noreply.github.com>
2021-01-01 03:02:10 +0200

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,63 @@
+clear
+
+echo -e """
+
+▒█▀▄▀█ ▒█▀▀█ ░░ ▒█░▒█ ▒█░▄▀
+
+▒█▒█▒█ ▒█▄▄▀ ▀▀ ▒█▀▀█ ▒█▀▄░
+
+▒█░░▒█ ▒█░▒█ ░░ ▒█░▒█ ▒█░▒█
+
+""" | lolcat -a -d 2
+
+printf "$0 \e[4m made by \e[1m\e[33m @MR-HK\e[0m\n"
+
+printf '\e[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m\n'
+
+printf '\e[32m[\e[39m*\e[32m]\e[36m     whats your name :- \e[0m' && read name
+
+printf '\e[35m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\e[0m\n'
+
+cd /data/data/com.termux/files/home && rm MR-HKjo.sh cd /data/data/com.termux/files/hom/MR-HK1 || cd /data/data/com.termux/files/home/MR-HK1 && rm MR-HKjo.sh && touch MR-HKjo.sh || cd /data/data/com.termux/files/home/MR-HK1 && touch MR-HKjo.sh
+
+echo -e """
+
+cowsay -f MR-HK.cow $name | lolcat -p
+
+#neofetch --ascii "$(fortune | cowsay -f MR-HK.cow $name )" | lolcat -p
+
+figlet $name
+
+PS1='\[\033[01;31m\]┌──\[\033[01;34m\][\[\033[01;32m\]\T\[\033[01;34m\]]—————————————\033[0;33m⟩\033[0;34m⟩\033[0;36m$name\033[0;34m⟨\033[0;33m⟨\[\033[01;34m\]———————————————\[\033[01;34m\][\[\033[01;32m\]2021\[\033[01;34m\]]—[\#]\[\033[0
+
+\[\033[01;31m\]¦
+
+\[\033[01;31m\]└─\[\033[01;34m\][ \[\033[01;31m\]TERMUX\[\033[01;34m\] ]\[\033[01;33m\] -\[\033[01;33m\]}\[\033[01;33m\] '
+
+""" > MR-HKjo.sh && cp MR-HKjo.sh /data/data/com.termux/files/home && cd /data/data/com.termux/files/home && cp MR-HKjo.sh .bashrc
+
+echo -e """
+
+         █
+
+ ████    █              ████
+
+█    █ ███ ███ ███ ███ █    █
+
+█ ██ █ █ █ █ █ █ █ ███ █ ██ █
+
+█ ████ █ █ █ █ █ █ █   █ ████
+
+█      ███ ███ █ █ ███ █
+
+ ███                    ███
+
+       ▗ ▌     ▌
+
+       ▜▘▛▌▀▌▛▌▙▘  ▌▌▛▌▌▌
+
+       ▐▖▌▌█▌▌▌▛▖  ▙▌▙▌▙▌
+
+                   ▄▌
+
+"""
